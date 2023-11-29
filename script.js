5d1eecd5e6d7ba519e8624db9829cce6ebd9d8f0
jacopobiagi <jacopo.biagi02@gmail.com>
2023-11-29 16:41:40 +0100

    Ottimizzazione ciclo for

--- script.js
+++ script.js
@@ -7,72 +7,55 @@ let start = document.getElementById("start");
 start.addEventListener("click",
 
     ()=>{
+        let cond = 0;
         let level = document.getElementById("selectLevel").value;
         grid.innerHTML = "";
         grid.classList.remove("d-none");
-        grid.classList.add("d-flex")
+        grid.classList.add("d-flex");
+
 
         //1 CASE: LIVELLO DIFFICIEL
         if(level == 1){
-            for(let i = 1; i <= 100; i++){
-
-                let element = document.createElement("div");
-                element.classList.add("content");
-                element.classList.add("content-1")
-                element.innerHTML = i
-            
-                element.addEventListener("click",
-                
-                    ()=>{
-                        console.log("hai cliccato l'elemento numero: "+ i);
-                        element.style.backgroundColor = "blue";
-                    }
-                )
-            
-                grid.append(element)
-            }
+           
+            cond = 100;
 
         // 2 CASE: LIVELLO MEDIO
         }else if(level == 2){
 
-            for(let i = 1; i <= 81; i++){
-
-                let element = document.createElement("div");
-                element.classList.add("content");
-                element.classList.add("content-2")
-                element.innerHTML = i
-            
-                element.addEventListener("click",
-                
-                    ()=>{
-                        console.log("hai cliccato l'elemento numero: "+ i);
-                        element.style.backgroundColor = "blue";
-                    }
-                )
-            
-                grid.append(element)
-            }
+           
+            cond = 81;
 
         //3 CASE: LIVELLO FACILE
         }else{
 
-            for(let i = 1; i <= 49; i++){
+            cond = 49;
+        }
 
-                let element = document.createElement("div");
-                element.classList.add("content");
+
+        for(let i = 1; i <= cond; i++){
+
+            let element = document.createElement("div");
+            element.classList.add("content");
+            
+            if(cond == 100){
+                element.classList.add("content-1")
+            }else if(cond == 81){
+                element.classList.add("content-2")
+            }else{
                 element.classList.add("content-3")
-                element.innerHTML = i
-            
-                element.addEventListener("click",
-                
-                    ()=>{
-                        console.log("hai cliccato l'elemento numero: "+ i);
-                        element.style.backgroundColor = "blue";
-                    }
-                )
-            
-                grid.append(element)
             }
+            
+            element.innerHTML = i
+        
+            element.addEventListener("click",
+            
+                ()=>{
+                    console.log("hai cliccato l'elemento numero: "+ i);
+                    element.style.backgroundColor = "blue";
+                }
+            )
+        
+            grid.append(element)
         }
     }
 
